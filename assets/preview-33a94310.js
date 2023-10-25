0d497887c45fc371462c5bcec640d981de10016d
asieraduriz <asier.aduriz@preply.com>
2023-10-25 16:46:20 +0200

    Updates

---
+++ assets/preview-33a94310.js
@@ -0,0 +1,2 @@
+const a={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},backgrounds:{default:"dark"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}}}};export{a as default};
+//# sourceMappingURL=preview-33a94310.js.map
